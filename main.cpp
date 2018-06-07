e61d3c0f535b31a50967e339dfb4b5222b5c48de
John Smith <unknown.prop@gmail.com>
2018-06-07 17:58:39 +0900

    SDL 초기화 및 CMakeLists 수정
    REF:
    https://www.youtube.com/watch?v=7sIBklOTImI
    https://github.com/brendan-w/collector/blob/master/CMakeLists.txt

--- main.cpp
+++ main.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+#include <SDL2/SDL.h>
+using namespace std;
+
+int main(int argc, char* argv[]) {
+
+    if(SDL_Init(SDL_INIT_VIDEO) < 0) {
+        cout << "SDL Init failed..." << endl;
+        return 1;
+    }
+
+    cout << "SDL Init Successed." << endl;
+
+    SDL_Quit();
+
     return 0;
 }
