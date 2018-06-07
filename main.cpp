6b71c8ea96f8c8d46ec4a536bad422920941b303
John Smith <unknown.prop@gmail.com>
2018-06-07 22:45:36 +0900

    Textures, Renderers and Buffers

--- main.cpp
+++ main.cpp
@@ -10,6 +10,8 @@ int main(int argc, char* argv[]) {
         cout << "SDL Init failed..." << endl;
         return 1;
     }
+
+    // https://wiki.libsdl.org/SDL_CreateWindow
     SDL_Window* window = SDL_CreateWindow("Particle Fire Explosion",
             SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
 
@@ -18,6 +20,36 @@ int main(int argc, char* argv[]) {
         SDL_Quit();
         return 2;
     }
+
+    // https://wiki.libsdl.org/SDL_CreateRenderer
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_PRESENTVSYNC);
+    if(renderer == NULL) {
+        printf("Could not create renderer: %s\n", SDL_GetError());
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 3;
+    }
+
+    // https://wiki.libsdl.org/SDL_CreateTexture
+    SDL_Texture* texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888,
+            SDL_TEXTUREACCESS_STATIC, SCREEN_WIDTH, SCREEN_HEIGHT);
+    if(texture == NULL) {
+        printf("Could not create texture: %s\n", SDL_GetError());
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 4;
+    }
+
+    // screen buffer
+    Uint32* buffer = new Uint32[SCREEN_WIDTH * SCREEN_HEIGHT];
+
+    memset(buffer, 0xFF, SCREEN_WIDTH * SCREEN_HEIGHT * sizeof(Uint32));
+
+    SDL_UpdateTexture(texture, NULL, buffer, SCREEN_WIDTH * sizeof(Uint32));
+    SDL_RenderClear(renderer);
+    SDL_RenderCopy(renderer, texture, NULL, NULL);
+    SDL_RenderPresent(renderer);
 
     bool quit = false;
 
@@ -34,6 +66,9 @@ int main(int argc, char* argv[]) {
         }
     }
 
+    delete [] buffer;
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyTexture(texture);
     SDL_DestroyWindow(window);
     SDL_Quit();
 
