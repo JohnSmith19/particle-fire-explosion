adeae22c3f1401d455cb6335e35c83f4859abfba
John Smith <unknown.prop@gmail.com>
2018-06-07 20:04:28 +0900

    Creating an SDL Window
    https://wiki.libsdl.org/SDL_CreateWindow

--- main.cpp
+++ main.cpp
@@ -3,14 +3,38 @@
 using namespace std;
 
 int main(int argc, char* argv[]) {
+    const int SCREEN_WIDTH = 800;
+    const int SCREEN_HEIGHT = 600;
 
     if(SDL_Init(SDL_INIT_VIDEO) < 0) {
         cout << "SDL Init failed..." << endl;
         return 1;
     }
+    SDL_Window* window = SDL_CreateWindow("Particle Fire Explosion",
+            SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
 
-    cout << "SDL Init Successed." << endl;
+    if(window == NULL) {
+        printf("Could not create window: %s\n", SDL_GetError());
+        SDL_Quit();
+        return 2;
+    }
 
+    bool quit = false;
+
+    SDL_Event event;
+    while(!quit) {
+        // Update particles
+        // Draw particles
+        // Check for messages/events
+
+        while(SDL_PollEvent(&event)) {
+            if(event.type == SDL_QUIT) {
+                quit = true;
+            }
+        }
+    }
+
+    SDL_DestroyWindow(window);
     SDL_Quit();
 
     return 0;
